8ec158fca1b35beaab1d152bd19d87e439bcfb58
Celina <luv.mins99@gmail.com>
2019-06-06 11:44:26 +0900

    * 3-1 add handlebars

---
+++ src/test/java/com/mins99/webservice/web/WebControllerTest.java
@@ -0,0 +1,29 @@
+package com.mins99.webservice.web;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class WebControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void mainPageLoading() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("x1우민 제대해라");
+    }
+}
